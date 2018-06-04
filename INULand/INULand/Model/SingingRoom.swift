87b7abba4f87902552b43913478c66797226d72d
Cho <choymoonk@gmail.com>
2018-06-04 21:33:22 +0900

    네트워크 구현, 버튼 연동 80% 완료

---
+++ INULand/INULand/Model/SingingRoom.swift
@@ -0,0 +1,9 @@
+//
+//  SingingRoom.swift
+//  INULand
+//
+//  Created by Cho on 2018. 6. 4..
+//  Copyright © 2018년 Cho. All rights reserved.
+//
+
+import Foundation
---
+++ INULand/INULand/NetworkCallback.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkCallback.swift
+//  INULand
+//
+//  Created by Cho on 2018. 6. 4..
+//  Copyright © 2018년 Cho. All rights reserved.
+//
+
+import Foundation
