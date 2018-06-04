82a61bd936e15ddd007eeb5a602782bd317d2b27
동균 <hdg9455@naver.com>
2018-06-04 20:25:31 +0900

    동균 - splash 및 로그인 구현

--- INULand/INULand/ProfileViewController.swift
+++ INULand/INULand/ProfileViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    @IBOutlet weak var profileCollectionView: UICollectionView!
+    
+    @IBOutlet weak var reservationCellView: UIView!
+    @IBOutlet weak var noShowCellView: UIView!
+    
+    @IBOutlet weak var reservationLocationLabel: UILabel!
+    @IBOutlet weak var reservationTimeLabel: UILabel!
+    @IBOutlet weak var noReservationLabel: UILabel!
+    @IBOutlet weak var reservationCancelButton: UIButton!
+    @IBOutlet weak var divisionLine: UIView!
+    
+    @IBOutlet weak var noShowCountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionViewInitialize()
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -23,51 +35,37 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-
-}
-
-extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 1
-        case 1:
-            return 2
-        
-        default:
-            return 0
-        }
-        
+    @IBAction func logOutButtonClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    
+    func collectionViewInitialize(){
+        reservationCellView.layer.borderWidth = 1.0
+        reservationCellView.layer.cornerRadius = 5
+        reservationCellView.layer.borderColor = UIColor.clear.cgColor
+        reservationCellView.layer.masksToBounds = true;
         
-        if indexPath.row == 1 {
-            let noShowCell = profileCollectionView.dequeueReusableCell(withReuseIdentifier: "ProfileNoShowCollectionViewCell", for: indexPath) as! ProfileNoShowCollectionViewCell
-            
-            return noShowCell
-
-        }else{
-            let noShowCell = profileCollectionView.dequeueReusableCell(withReuseIdentifier: "ProfileNoShowCollectionViewCell", for: indexPath) as! ProfileNoShowCollectionViewCell
-            
-            return noShowCell
-
-        }
+        reservationCellView.layer.shadowColor = UIColor.lightGray.cgColor
+        reservationCellView.layer.shadowRadius = 1.5
+        reservationCellView.layer.shadowOffset = CGSize(width: 0, height: 3.0)
+        reservationCellView.layer.shadowOpacity = 0.4
+        reservationCellView.layer.masksToBounds = false
+        reservationCellView.layer.shadowPath = UIBezierPath(roundedRect: reservationCellView.bounds, cornerRadius: 5).cgPath
+        
+        noShowCellView.layer.borderWidth = 1.0
+        noShowCellView.layer.cornerRadius = 5
+        noShowCellView.layer.borderColor = UIColor.clear.cgColor
+        noShowCellView.layer.masksToBounds = true;
+        
+        noShowCellView.layer.shadowColor = UIColor.lightGray.cgColor
+        noShowCellView.layer.shadowRadius = 1.5
+        noShowCellView.layer.shadowOffset = CGSize(width: 0, height: 3.0)
+        noShowCellView.layer.shadowOpacity = 0.4
+        noShowCellView.layer.masksToBounds = false
+        noShowCellView.layer.shadowPath = UIBezierPath(roundedRect: noShowCellView.bounds, cornerRadius: 5).cgPath
     }
+    
 }
 
-
-extension ProfileViewController {
-    func collectionViewInitialize() {
-        self.profileCollectionView.dataSource = self
-        self.profileCollectionView.delegate = self
-        
-        self.profileCollectionView.register(UINib(nibName:"ProfileNoShowCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "ProfileNoShowCollectionViewCell")
-        
-    }
-}
